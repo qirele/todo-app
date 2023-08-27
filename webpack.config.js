221e9998616766e572c7ba7f687a7406c07788db
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2023-08-27 18:31:22 +0200

    add webpack boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+
+  entry: './src/index.js',
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo App',
+    }),
+  ],
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    static: './dist',
+    open: {
+      app: {
+        name: 'google-chrome',
+      },
+    },
+  },
+};
