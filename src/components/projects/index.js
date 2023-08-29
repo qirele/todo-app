980bb98ddcf317aecaa1bed6917fbf60146f3205
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2023-08-29 15:22:15 +0200

    transfer modifying todo-items logic to the entry js file

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,58 +1,24 @@
-const body = document.querySelector("body");
+export default function createProjects(projects) {
+  const main = document.createElement("main");
 
-function createDivProject(projects, main) {
   projects.forEach((project, projectIdx) => {
     const div = document.createElement("div");
-    div.addEventListener("click", handleDivClick);
     const h1 = document.createElement("h1");
     h1.textContent = project.getTitle();
     h1.dataset.projectIdx = projectIdx;
     div.appendChild(h1);
 
-    project.getTodos().forEach((todo, idx) => {
+    project.getTodos().forEach((todo, todoIdx) => {
       const p = document.createElement("p");
       p.textContent = todo.getTitle();
-      p.dataset.idx = idx;
+      p.dataset.idx = todoIdx;
       p.dataset.projectIdx = projectIdx;
       div.appendChild(p);
     });
+
     main.appendChild(div);
+
   });
 
-  function handleDivClick(e) {
-    const todoPara = e.target;
-    let todoIdx = e.target.dataset.idx;
-    let projectIdx = e.target.dataset.projectIdx;
-    if (todoIdx) { // clicked on a todo item
-      todoPara.textContent = "";
-      const input = document.createElement("input");
-      const btn = document.createElement("button");
-      btn.textContent = "Change";
-      btn.addEventListener("click", () => handleTodoClick(input));
-      todoPara.appendChild(input);
-      todoPara.appendChild(btn);
-    } else if (projectIdx) { // clicked on a h1, ie. project title
-
-    }
-
-    function handleTodoClick(input) {
-      const theTodoItem = projects[projectIdx].getTodos()[todoIdx];
-      theTodoItem.setTitle(input.value === "" ? theTodoItem.getTitle() : input.value);
-      todoPara.textContent = theTodoItem.getTitle();
-    }
-
-  }
-
-}
-
-export default function (projects) {
-  let main = body.querySelector("main");
-  if (main === null) {
-    main = document.createElement("main");
-    createDivProject(projects, main);
-    body.appendChild(main);
-  } else {
-    main.textContent = "";
-    createDivProject(projects, main);
-  }
+  return main;
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 import createTodo from './components/todoItem';
 import createProject from './components/project';
-import createProjectsView from './components/projects';
+import createProjectsMain from './components/projects';
 import createUI from './components/ui';
 
 let currentProject;
@@ -12,23 +12,63 @@ projects.push(currentProject);
 
 const body = document.querySelector("body");
 const { div, input, addTodoBtn, addProjectBtn } = createUI();
+const main = createProjectsMain(projects);
 body.appendChild(div);
+body.appendChild(main);
+attachListenersForTodos();
 
 addTodoBtn.addEventListener("click", handleAddTodo)
-
 addProjectBtn.addEventListener("click", handleAddProject)
 
-createProjectsView(projects);
 
 function handleAddTodo() {
   const item1 = createTodo(input.value, "description");
   currentProject.addTodo(item1);
-  createProjectsView(projects);
+  replaceMain();
 }
 
 function handleAddProject() {
   const proj = createProject(input.value);
   currentProject = proj;
   projects.push(currentProject);
-  createProjectsView(projects);
+  replaceMain();
+}
+
+function replaceMain() {
+  const newMain = createProjectsMain(projects);
+  const oldMain = document.querySelector("main");
+  oldMain.remove();
+  body.appendChild(newMain);
+  attachListenersForTodos();
+}
+
+function attachListenersForTodos() {
+  const divs = document.querySelectorAll("main > div");
+  divs.forEach(div => div.addEventListener("click", handleDivClick) );
+}
+
+function handleDivClick(e) {
+
+  const todoPara = e.target;
+  let todoIdx = e.target.dataset.idx;
+  let projectIdx = e.target.dataset.projectIdx;
+
+  if (todoIdx) { // clicked on a todo item
+    todoPara.textContent = "";
+    const input = document.createElement("input");
+    const btn = document.createElement("button");
+    btn.textContent = "Change";
+    btn.addEventListener("click", () => handleTodoClick(input));
+    todoPara.appendChild(input);
+    todoPara.appendChild(btn);
+  } else if (projectIdx) { // clicked on a h1, ie. project title
+    // TODO(Kiril): add an input and a button to change project title
+  }
+
+  function handleTodoClick(input) {
+    const theTodoItem = projects[projectIdx].getTodos()[todoIdx];
+    theTodoItem.setTitle(input.value === "" ? theTodoItem.getTitle() : input.value);
+    todoPara.textContent = theTodoItem.getTitle();
+  }
+
 }
